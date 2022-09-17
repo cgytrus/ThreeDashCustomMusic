66bf459ae5dfde90bec84061248424f8cff8e751
ConfiG <cgytrus@cgyt.ru>
2022-09-17 22:37:43 +0300

    update 6dash to 0.3.0

--- ThreeDashCustomMusic/src/Plugin.cs
+++ ThreeDashCustomMusic/src/Plugin.cs
@@ -5,7 +5,7 @@ using SixDash;
 namespace ThreeDashCustomMusic;
 
 [BepInPlugin("mod.cgytrus.plugins.3dashcustommusic", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-[BepInDependency("mod.cgytrus.plugins.sixdash", "0.2.0")]
+[BepInDependency("mod.cgytrus.plugins.sixdash", "0.3.0")]
 public class Plugin : BaseUnityPlugin {
     private void Awake() {
         CustomMusic.Setup(Logger);
